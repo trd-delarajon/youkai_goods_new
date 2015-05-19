808f3d875cbd4fcb40297c78f542b48c35a050df
timeriverdesign <timeriverdesign@192.168.1.63>
2015-05-19 08:58:27 +0800

    the

--- youkai_goods_static/config.php
+++ youkai_goods_static/config.php
@@ -1,6 +1,6 @@
 <?php 
 
-	require_once("smarty/libs/Smarty.class.php");
+	require_once("Archive/smarty/libs/Smarty.class.php");
 
 	$smarty = new Smarty(); //new object
 	$smarty->template_dir = 'templates';
